1384087e14e158ed6f699e5ca24809430d6de922
Giorgio <tran.giorgio@gmail.com>
2024-11-12 23:08:28 -1000

    Add choropleth sample

--- coffee-data/src/app/page.tsx
+++ coffee-data/src/app/page.tsx
@@ -1,3 +1,55 @@
+"use client";
+
+import Globe from "react-globe.gl";
+import { useState, useEffect, useMemo } from "react";
+import * as d3 from "d3";
+
 export default function Page() {
-  return <h1>Welcome to Coffee Data Visualization App</h1>
+  const [countries, setCountries] = useState({ features: [] });
+  const [hoverD, setHoverD] = useState();
+
+  useEffect(() => {
+    // load data
+    fetch(
+      "https://raw.githubusercontent.com/vasturiano/react-globe.gl/refs/heads/master/example/datasets/ne_110m_admin_0_countries.geojson"
+    )
+      .then((res) => res.json())
+      .then(setCountries);
+  }, []);
+
+  const colorScale = d3.scaleSequentialSqrt(d3.interpolateYlOrRd);
+
+  // GDP per capita (avoiding countries with small pop)
+  const getVal = (feat) =>
+    feat.properties.GDP_MD_EST / Math.max(1e5, feat.properties.POP_EST);
+
+  const maxVal = useMemo(
+    () => Math.max(...countries.features.map(getVal)),
+    [countries]
+  );
+  colorScale.domain([0, maxVal]);
+
+  return (
+    <Globe
+      globeImageUrl="//unpkg.com/three-globe/example/img/earth-night.jpg"
+      backgroundImageUrl="//unpkg.com/three-globe/example/img/night-sky.png"
+      lineHoverPrecision={0}
+      polygonsData={countries.features.filter(
+        (d) => d.properties.ISO_A2 !== "AQ"
+      )}
+      polygonAltitude={(d) => (d === hoverD ? 0.12 : 0.06)}
+      polygonCapColor={(d) =>
+        d === hoverD ? "steelblue" : colorScale(getVal(d))
+      }
+      polygonSideColor={() => "rgba(0, 100, 0, 0.15)"}
+      polygonStrokeColor={() => "#111"}
+      polygonLabel={({ properties: d }) => `
+      <b>${d.ADMIN} (${d.ISO_A2}):</b> <br />
+      GDP: <i>${d.GDP_MD_EST}</i> M$<br/>
+      Population: <i>${d.POP_EST}</i>
+    `}
+      onPolygonHover={setHoverD}
+      polygonsTransitionDuration={300}
+    />
+  );
 }
