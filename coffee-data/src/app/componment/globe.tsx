46104bdfffcaf99ed028e2136ed4c9977097d1e5
Giorgio <tran.giorgio@gmail.com>
2024-11-14 12:29:09 -1000

    Add export, import, and production into geojson

--- coffee-data/src/app/componment/globe.tsx
+++ coffee-data/src/app/componment/globe.tsx
@@ -1,58 +1,60 @@
 "use client";
-import {useState, useEffect, useMemo} from "react";
+import { useState, useEffect, useMemo } from "react";
 import dynamic from "next/dynamic";
 import * as d3 from "d3";
 
 // Dynamically import the Globe component with SSR disabled
-const Globe = dynamic(() => import("react-globe.gl"), {ssr: false});
+const Globe = dynamic(() => import("react-globe.gl"), { ssr: false });
 
 export default function GlobeComponent() {
-    const [countries, setCountries] = useState({features: []});
-    const [hoverD, setHoverD] = useState();
+  const [countries, setCountries] = useState({ features: [] });
+  const [hoverD, setHoverD] = useState();
 
-    useEffect(() => {
-        // load data from the local file in the public directory
-        fetch("/data/ne_110m_admin_0_countries.geojson")
-            .then((res) => res.json())
-            .then(setCountries);
-    }, []);
+  useEffect(() => {
+    // load data from the local file in the public directory
+    fetch("/data/ne_110m_admin_0_countries.geojson")
+      .then((res) => res.json())
+      .then(setCountries);
+  }, []);
 
-    const colorScale = d3.scaleSequentialSqrt(d3.interpolateYlOrRd);
+  const colorScale = d3.scaleSequentialSqrt(d3.interpolateYlOrRd);
 
-    // Calculate GDP per capita (avoiding countries with small populations)
-    const getVal = (feat) =>
-        feat.properties.GDP_MD_EST / Math.max(1e5, feat.properties.POP_EST);
+  // Calculate GDP per capita (avoiding countries with small populations)
+  const getVal = (feat) =>
+    feat.properties.GDP_MD_EST / Math.max(1e5, feat.properties.POP_EST);
 
-    // Find the max value to scale the color range
-    const maxVal = useMemo(
-        () => Math.max(...countries.features.map(getVal)),
-        [countries]
-    );
-    colorScale.domain([0, maxVal]);
+  // Find the max value to scale the color range
+  const maxVal = useMemo(
+    () => Math.max(...countries.features.map(getVal)),
+    [countries]
+  );
+  colorScale.domain([0, maxVal]);
 
-    return (
-        <div>
-            <Globe
-                globeImageUrl="//unpkg.com/three-globe/example/img/earth-night.jpg"
-                backgroundImageUrl="//unpkg.com/three-globe/example/img/night-sky.png"
-                lineHoverPrecision={0}
-                polygonsData={countries.features.filter(
-                    (d) => d.properties.ISO_A2 !== "AQ"
-                )}
-                polygonAltitude={(d) => (d === hoverD ? 0.12 : 0.06)}
-                polygonCapColor={(d) =>
-                    d === hoverD ? "steelblue" : colorScale(getVal(d))
-                }
-                polygonSideColor={() => "rgba(0, 100, 0, 0.15)"}
-                polygonStrokeColor={() => "#111"}
-                polygonLabel={({properties: d}) => `
+  console.log(countries.features.filter((d) => d.properties.ISO_A2 !== "AQ"))
+
+  return (
+    <div>
+      <Globe
+        globeImageUrl="//unpkg.com/three-globe/example/img/earth-night.jpg"
+        backgroundImageUrl="//unpkg.com/three-globe/example/img/night-sky.png"
+        lineHoverPrecision={0}
+        polygonsData={countries.features.filter(
+          (d) => d.properties.ISO_A2 !== "AQ"
+        )}
+        polygonAltitude={(d) => (d === hoverD ? 0.12 : 0.06)}
+        polygonCapColor={(d) =>
+          d === hoverD ? "steelblue" : colorScale(getVal(d))
+        }
+        polygonSideColor={() => "rgba(0, 100, 0, 0.15)"}
+        polygonStrokeColor={() => "#111"}
+        polygonLabel={({ properties: d }) => `
           <b>${d.ADMIN} (${d.ISO_A2}):</b> <br />
           GDP: <i>${d.GDP_MD_EST}</i> M$<br/>
           Population: <i>${d.POP_EST}</i>
         `}
-                onPolygonHover={setHoverD}
-                polygonsTransitionDuration={300}
-            />
-        </div>
-    );
+        onPolygonHover={setHoverD}
+        polygonsTransitionDuration={300}
+      />
+    </div>
+  );
 }
